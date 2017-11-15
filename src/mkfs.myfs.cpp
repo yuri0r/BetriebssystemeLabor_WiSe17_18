fb6e70e6a6a6a1afa8a6bf2f2c0bd761afd4b04c
yuri0r <macktrinz@yahoo.de>
2017-11-15 16:53:45 +0100

    just some argument testing

--- src/mkfs.myfs.cpp
+++ src/mkfs.myfs.cpp
@@ -9,10 +9,15 @@
 #include "myfs.h"
 #include "blockdevice.h"
 #include "macros.h"
+#include <iostream>
 
 int main(int argc, char *argv[]) {
 
     // TODO: Implement file system generation & copying of files here
-    
+        std::cout << argc << std::endl ;
+
+        for(int i=0;i<argc;i++){
+            std::cout << argv[i] << std::endl ;
+        }
     return 0;
 }
