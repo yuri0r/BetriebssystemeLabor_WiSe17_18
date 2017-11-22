16179b47249fa35acff17e31a758d0f123a280ac
yuri <macktrinz@yahoo.de>
2017-11-22 22:38:52 +0100

    work in progress still need to figure out how to place int values into buffer

--- src/mkfs.myfs.cpp
+++ src/mkfs.myfs.cpp
@@ -17,7 +17,17 @@
 #define MAX_FILES 64
 
 #define SUPER_BLOCK_ADRESS 0
-#define SUPERBLOCK_SIZE 1
+#define SUPER_BLOCK_SIZE 1
+#define SUPER_BLOCK_NAME "myFS"
+
+#define SUPER_BLOCK_NAME_POSITION 0
+#define SUPER_BLOCK_BLOCK_SIZE_POSITION 32
+#define SUPER_BLOCK_MAX_FILE_SIZE_POSITION 64
+#define SUPER_BLOCK_MAX_FILES_POSITION 96
+#define SUPER_BLOCK_ROOT_ADRESS_POSITION 128
+#define SUPER_BLOCK_INODES_POSITION 160
+#define SUPER_BLOCK_FAT_ADRESS_POSITION 192
+#define SUPER_BLOCK_DATA_ADRESS_POSITION 224
 
 #define ROOT_ADRESS 1
 #define ROOT_SIZE 1
@@ -46,24 +56,33 @@ int main(int argc, char *argv[]) {
     BlockDevice *bd  = new BlockDevice(BLOCK_SIZE);
     bd->create(containerPath);
 
-    for(int i=0;i<argc;i++){
+    for(unsigned int i=2; i<argc; i++){
         std::cout << "Argument " << i << ": "  <<  argv[i] << std::endl;
         bd->write(DATA_ADRESS + i, argv[i]);         //argv[] can not exceed one block in size
     }
 
     // TODO create Superblock
-    char* superBlockContent  = "our nice super file system of doom and citty cats <333";
     char null = NULL;
     char* buffer = (char*)malloc(BLOCK_SIZE);
+    const char* blockSizePosition = (std::to_string(SUPER_BLOCK_BLOCK_SIZE_POSITION)).c_str();
+    unsigned int offset = 0;
 
-    for(int i = 0; i < BLOCK_SIZE + strlen(superBlockContent); i++) {
-        if(i < strlen(superBlockContent)){
-            buffer[i] = superBlockContent[i];
-        } else {
-            buffer[i] = null;
-        }
+    //fill with super block information
+    for (unsigned int i = 0; i < sizeof(SUPER_BLOCK_NAME); i++ ){
+        offset = SUPER_BLOCK_NAME_POSITION;
+        buffer[i + offset] = SUPER_BLOCK_NAME[i + offset];
     }
 
+    for (unsigned int i = 0; i < sizeof(blockSizePosition); i++ ){
+        buffer[i + SUPER_BLOCK_BLOCK_SIZE_POSITION] = blockSizePosition[i + SUPER_BLOCK_BLOCK_SIZE_POSITION];
+        std::cout << buffer[i+32] <<"yes pls";
+    }
+
+    for(unsigned int i = 0; i < BLOCK_SIZE * SUPER_BLOCK_SIZE; i++) {    
+        //buffer[i] = null;
+    }
+    
+    std::cout << buffer << std::endl << blockSizePosition;
     bd->write(0,buffer);
 
     // End of create Superblock
