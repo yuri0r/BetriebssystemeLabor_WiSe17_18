84a6249cea95eb2e6841c353b40bc363737740b9
yuri <macktrinz@yahoo.de>
2017-12-02 22:57:32 +0100

    untested create inode method has been added

--- src/mkfs.myfs.cpp
+++ src/mkfs.myfs.cpp
@@ -27,8 +27,7 @@
 #define INODES_ADRESS 2
 #define INODES_SIZE MAX_FILES
 
-
-#define FAT_ADRESS  65
+#define FAT_ADRESS 65
 #define FAT_SIZE (MAX_FILE_SIZE / BLOCK_SIZE * MAX_FILES)
 
 #define DATA_ADRESS (FAT_ADRESS + FAT_SIZE)
@@ -44,26 +43,26 @@ struct SuperBlock
     int dataAdress;
 };
 
-struct Inode // Bytes: 256  + 3 + 4 + 1 + 4 + 4 + 4 + 4 + 32 + 32 = 344
+struct Inode // Bytes: 256  + 3 + 4 + 1 + 4 + 4 + 4 + 4 + 32 + 32 = 344 @curvel
 {
-  	char fileName [256] = { };		// act of pure rebelion! (also 255 is just ugly)
-  	char fileExtension [3] = { }; 	// file "type" (eg .txt)
-    uint32_t fileSize;				// size of file
-    uint8_t mode; 					// rwx
-    uint32_t atime;     			// last access
-    uint32_t mtime;     			// last modification
-    uint32_t ctime;     			// last modification of status
-    int firstFatEntry;     			// pointer to fat
-  	char userID [32] = { };			// id Of user
-  	char groupID [32] = { };		// id of group
+    char fileName[256] = {};    // act of pure rebelion! (also 255 is just ugly) @yuri
+    char fileExtension[3] = {}; // file "type" (eg .txt)
+    uint32_t fileSize;          // size of file
+    uint8_t mode;               // rwx
+    uint32_t atime;             // last access
+    uint32_t mtime;             // last modification
+    uint32_t ctime;             // last modification of status
+    int firstFatEntry;          // pointer to fat
+    char userID[32];       // id Of user
+    char groupID[32];      // id of group
 };
 
-struct FatBlock 
+struct FatBlock
 {
-    int destination [16] = { };
+    int destination[16] = {};
 } fatBlock;
 
-BlockDevice* bd = new BlockDevice(BLOCK_SIZE);
+BlockDevice *bd = new BlockDevice(BLOCK_SIZE);
 
 void initSuperBlock()
 {
@@ -87,6 +86,41 @@ void initSuperBlock()
     }
 }
 
+void createInode(int inodeIndex,
+                 char **fileName,
+                 char **fileExtension,
+                 uint32_t *fileSize,
+                 uint8_t *mode,
+                 uint32_t *atime,
+                 uint32_t *mtime,
+                 uint32_t *ctime,
+                 int *firstFatEntry,
+                 char **userID,
+                 char **groupID)
+{
+    Inode *inode = (Inode *)malloc(BLOCK_SIZE);
+
+    strcpy( inode->fileName, *fileName);
+    strcpy( inode->fileExtension, *fileExtension);
+    inode->fileSize = *fileSize;
+    inode->mode = *mode;
+    inode->atime = *atime;
+    inode->mtime = *mtime;
+    inode->ctime = *ctime;
+    inode->firstFatEntry = *firstFatEntry;
+    strcpy( inode->userID, *userID);
+    strcpy( inode->groupID, *groupID);
+
+    if (inodeIndex)
+    {
+        bd->write(inodeIndex + INODES_ADRESS, (char *)inode);
+    }
+    else
+    {
+        std::cout << "ERROR not in Inode Space" ;
+    }
+}
+
 void writeFat(int start, int destination)
 {
     int fatBlockCount = (start - DATA_ADRESS) / 16;
@@ -94,44 +128,49 @@ void writeFat(int start, int destination)
 
     FatBlock *fb = (FatBlock *)malloc(BLOCK_SIZE);
 
-    bd->read(FAT_ADRESS + fatBlockCount, (char *) fb);
+    bd->read(FAT_ADRESS + fatBlockCount, (char *)fb);
 
     fb->destination[destinationCount] = destination;
 
-    bd->write(FAT_ADRESS + fatBlockCount, (char *) fb);
+    bd->write(FAT_ADRESS + fatBlockCount, (char *)fb);
 }
 
-int readFat(int position) {
+int readFat(int position)
+{
     int fatBlockCount = position / 16;
     int destinationCount = position % 16;
 
     FatBlock *fb = (FatBlock *)malloc(BLOCK_SIZE);
 
-    bd->read(FAT_ADRESS + fatBlockCount, (char *) fb);
+    bd->read(FAT_ADRESS + fatBlockCount, (char *)fb);
 
     return fb->destination[destinationCount];
 }
 
-void dataCreation(int argc, char* argv[])
+void dataCreation(int argc, char *argv[])
 {
     int addressCounter = 0;
-    for(int i=2;i<argc;i++){
-        std::streampos size;                                   
-        std::ifstream file (argv[i], std::ios::in|std::ios::binary|std::ios::ate); //openfile
-        if(file.is_open()){
+    for (int i = 2; i < argc; i++)
+    {
+        std::streampos size;
+        std::ifstream file(argv[i], std::ios::in | std::ios::binary | std::ios::ate); //openfile
+        if (file.is_open())
+        {
             size = file.tellg();
-            char* filebuffer = (char*)malloc(size);                             //save file localy
+            char *filebuffer = (char *)malloc(size); //save file localy
             file.seekg(0, std::ios::beg);
             file.read(filebuffer, size);
             file.close();
-            for(int i=0; i < size; i+=BLOCK_SIZE){ 
-                char* filewriter = filebuffer + i;
+            for (int i = 0; i < size; i += BLOCK_SIZE)
+            {
+                char *filewriter = filebuffer + i;
                 bd->write(DATA_ADRESS + addressCounter, filewriter);
-                int j =  i + BLOCK_SIZE;
-                if (j < size) writeFat(DATA_ADRESS + addressCounter, DATA_ADRESS + addressCounter + 1);
+                int j = i + BLOCK_SIZE;
+                if (j < size)
+                    writeFat(DATA_ADRESS + addressCounter, DATA_ADRESS + addressCounter + 1);
                 addressCounter++;
             }
-            std::cout<<size<<std::endl;
+            std::cout << size << std::endl;
             free(filebuffer);
         }
     }
@@ -153,10 +192,10 @@ int main(int argc, char *argv[])
     containerPath = argv[1];
     bd->create(containerPath);
 
-    for(int i=0;i<argc;i++){
-        std::cout << "Argument " << i << ": "  <<  argv[i] << std::endl;
+    for (int i = 0; i < argc; i++)
+    {
+        std::cout << "Argument " << i << ": " << argv[i] << std::endl;
         //bd->write(DATA_ADRESS + i, argv[i]);         //now done in DATA (@tristan)
-
     }
 
     // TODO create Superblock (done)
@@ -175,8 +214,8 @@ int main(int argc, char *argv[])
     // End of create INODES
 
     // TODO create DATA
-    dataCreation(argc,argv);
-    
+    dataCreation(argc, argv);
+
     // End of create DATA
 
     // TODO Calculate size of Binary file
